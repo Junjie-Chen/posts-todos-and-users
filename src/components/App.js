184b348507de0cab8ece59cf4638f6e56566b982
Junjie Chen <junjie.chen18@gmail.com>
2020-06-05 10:56:40 -0400

    feat(component): render posts, todos and users buttons in browser

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <button>Posts</button>
+        <button>Todos</button>
+        <button>Users</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
