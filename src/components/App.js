4c294007cb9f63e9435bfe09118fc9a3481d1c83
Junjie Chen <junjie.chen18@gmail.com>
2020-06-05 11:25:58 -0400

    feat(hook): add onClick event and its handler to posts button

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import ResourceList from './ResourceList';
 
 const App = () => {
-  const [resource] = useState('posts');
+  const [resource, setResource] = useState('posts');
 
   return (
     <div>
       <div>
-        <button>Posts</button>
+        <button onClick={() => setResource('posts')}>Posts</button>
         <button>Todos</button>
         <button>Users</button>
       </div>
