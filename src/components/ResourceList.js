417d8d6636db32997f61141dca3e89fdc8ffa944
Junjie Chen <junjie.chen18@gmail.com>
2020-06-05 11:22:33 -0400

    feat(component): render resource list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -2,9 +2,15 @@ import React from 'react';
 import useResources from '../hook/useResources';
 
 const ResourceList = ({ resource }) => {
-  useResources(resource);
+  const resources = useResources(resource);
 
-  return <div>Resource List</div>;
+  return (
+    <ul>
+      {
+        resources.map(resource => <li key={resource.id}>{resource.title || resource.name}</li>)
+      }
+    </ul>
+  );
 };
 
 export default ResourceList;
