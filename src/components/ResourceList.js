064b3910f21615bcb822f395317106b3bb86e51d
Junjie Chen <junjie.chen18@gmail.com>
2020-06-05 11:20:12 -0400

    feat(component): destructure props object

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import useResources from '../hook/useResources';
 
-const ResourceList = props => {
-  useResources(props.resource);
+const ResourceList = ({ resource }) => {
+  useResources(resource);
 
   return <div>Resource List</div>;
 };
